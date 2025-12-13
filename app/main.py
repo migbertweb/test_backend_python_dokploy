60a8b531ad20800cf8d960b2aa19892b2668151c
migbertweb <migbertyanez@disroot.org>
2025-12-13 12:55:41 -0300

    añadido middleware cors origins para el dominio de dokploy

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ app.add_middleware(LoggingMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://testdeploy-frontend-g0mwox-ea1ec8-204-216-146-246.traefik.me"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
